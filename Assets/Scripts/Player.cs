ae06ca60faef56b936b2ffc3ec38e4d4ea8d414c
HaciCan29 <beyazs2013@yandex.com>
2025-09-24 18:14:13 +0300

    nad
    dfdfd

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+   
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
